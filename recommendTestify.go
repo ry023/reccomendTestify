a952266822b87d0f16f74979fb4dccca4145f685
ry023 <ryosuke.tanaka@pepabo.com>
2023-06-02 12:31:22 +0900

    First base impl

--- recommendTestify.go
+++ recommendTestify.go
@@ -2,15 +2,16 @@ package recommendTestify
 
 import (
 	"go/ast"
+	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-const doc = "recommendTestify is ..."
+const doc = `recommendTestify is a linter that outputs a message recommending the use of Testify when testing.T is used directly.
+This is a unique rule in hosborn development.`
 
-// Analyzer is ...
 var Analyzer = &analysis.Analyzer{
 	Name: "recommendTestify",
 	Doc:  doc,
@@ -21,20 +22,59 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-
-	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
-	}
-
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+	for _, method := range findMethods(pass) {
+		if method.ReceiverType.String() == "*testing.T" {
+			switch method.MethodIdent.Name {
+			case "Errorf":
+				pass.Reportf(method.CallExpr.Pos(), "We recommend using Testify (github.com/stretchr/testify). Use Testify's assertion methods or wrap by `assert.Failf` method.")
+			case "Error":
+				pass.Reportf(method.CallExpr.Pos(), "We recommend using Testify (github.com/stretchr/testify). Use Testify's assertion methods or wrap by `assert.Fail` method.")
+			case "Fatal":
+				pass.Reportf(method.CallExpr.Pos(), "We recommend using Testify (github.com/stretchr/testify). Use Testify's assertion methods or wrap by `assert.FailNow` method.")
+			case "Fatalf":
+				pass.Reportf(method.CallExpr.Pos(), "We recommend using Testify (github.com/stretchr/testify). Use Testify's assertion methods or wrap by `assert.FailNowf` method.")
 			}
 		}
-	})
-
+	}
 	return nil, nil
+}
+
+type MethodCall struct {
+	CallExpr     *ast.CallExpr
+	ReceiverExpr ast.Expr
+	ReceiverType types.Type
+	MethodIdent  *ast.Ident
+}
+
+func findMethods(pass *analysis.Pass) []*MethodCall {
+	methods := []*MethodCall{}
+
+	// Find functions or methods calling
+	nodeFilter := []ast.Node{
+		(*ast.CallExpr)(nil),
+	}
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		callExpr := n.(*ast.CallExpr)
+
+		// Check if the node is method
+		methodExpr, isMethod := callExpr.Fun.(*ast.SelectorExpr)
+		if !isMethod {
+			return
+		}
+
+		receiverExpr := methodExpr.X
+		methodIdent := methodExpr.Sel
+
+		methods = append(
+			methods,
+			&MethodCall{
+				CallExpr:     callExpr,
+				ReceiverExpr: receiverExpr,
+				ReceiverType: pass.TypesInfo.TypeOf(receiverExpr),
+				MethodIdent:  methodIdent,
+			},
+		)
+	})
+	return methods
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,12 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import (
+	"testing"
+)
+
+func TestSomething(t *testing.T) {
+	t.Errorf("Something Error: %s", "foobar") // want "assert.Failf"
+	t.Error("Something Error")                // want "assert.Fail"
+	t.Fatalf("Something Error: %s", "foobar") // want "assert.FailNowf"
+	t.Fatal("Something Error")                // want "assert.FailNow"
 }
